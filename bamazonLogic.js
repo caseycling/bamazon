8728ac200e63b4966e150cbf32a8f28989e25d11
Casey <caseyclinga@gmail.com>
2019-05-11 17:45:03 -0700

    created prompt which asks user for product id and quantity of the product they would like to purchase

--- bamazonLogic.js
+++ bamazonLogic.js
@@ -3,6 +3,7 @@ var mysql = require("mysql")
 //Require inquirer
 var inquirer = require("inquirer")
 
+//Create connection with mysql
 var connection = mysql.createConnection({
     host: "localhost",
   
@@ -24,13 +25,35 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId);
     afterConnection();
 });
-
+//Function which logs all the information from the products table in the MySQL database
 function afterConnection() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         console.log(res);
+        purchaseItem();
         connection.end();
     });
+}
+
+//Prompt which asks user which id of the item they'd like to buy and the quantity
+function purchaseItem() {
+    inquirer
+      .prompt([
+        {
+            name: "productId",
+            type: "input",
+            message: "What is the id of the production you would like to purchase?",
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "Please enter the amount you would like to purchase"
+        }
+    ])
+    .then(function(answer) {
+        console.log(answer.productId)
+        console.log(answer.quantity)
+    })
 }
 
 
