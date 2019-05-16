fcd6b8cbc2c1bf8b37233188dd0656058c43e090
Casey <caseyclinga@gmail.com>
2019-05-16 15:59:41 -0700

    added displayLowInventory function to show products with less than five remaining

--- bamazonManager.js
+++ bamazonManager.js
@@ -5,26 +5,26 @@ var inquirer = require("inquirer")
 
 //Create connection with mysql
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "$3ndNudes",
-  database: "bamazon_db"
+    // Your password
+    password: "$3ndNudes",
+    database: "bamazon_db"
 });
 
 //Connect and use callback with MySQL and call managerOptions function
 connection.connect(function (err) {
     if (err) {
-      throw err;
+        throw err;
     }
     console.log("connected as id " + connection.threadId);
-  
+
     managerOptions();
 });
 
@@ -35,49 +35,70 @@ function managerOptions() {
         type: "list",
         message: "Hello corporate overlord. What would you like to do today?",
         choices: [
-        "View products for sale", 
-        "View low inventory", 
-        "Add to inventroy", 
-        "Add new product"
+            "View products for sale",
+            "View low inventory",
+            "Add to inventroy",
+            "Add new product"
         ]
     })
-    .then(function (answer) {
-        if(answer.managerChoice === "View products for sale") {
-            displayInventory();
-        }
-        else if(answer.managerChoice === "View low inventory") {
+        .then(function (answer) {
+            if (answer.managerChoice === "View products for sale") {
+                displayInventory();
+            }
+            else if (answer.managerChoice === "View low inventory") {
+                displayLowInventory();
+            }
+            else if (answer.managerChoice === "Add to inventory") {
+                console.log("yess")
+            }
+            else (answer.managerChoice === "Add new product")
             console.log("yess")
-        }
-        else if(answer.managerChoice === "Add to inventory") {
-            console.log("yess")
-        }
-        else (answer.managerChoice === "Add new product") 
-            console.log("yess")
-    })
+        })
 }
 
 //Function which logs all the information from the products table in the MySQL database
 function displayInventory() {
     connection.query("SELECT * FROM products", function (err, res) {
-      if (err) throw err;
-      //Obligatory welcome message
-      console.log("-------------------------------------------------------")
-      console.log("Here you are, m'lord")
-      //Loop through each item in the products table
-      for (var i = 0; i < res.length; i++) {
+        if (err) throw err;
+        //Obligatory welcome message
         console.log("-------------------------------------------------------")
-        console.log("Product name: " + res[i].product_name);
-        console.log("Department: " + res[i].department_name);
-        console.log("Price: $" + res[i].price);
-        console.log("Product ID: " + res[i].item_id);
-        if (res[i].stock_quantity < 10) {
-          console.log("Less than 10 remaining!")
+        console.log("Here you are, m'lord")
+        //Loop through each item in the products table
+        for (var i = 0; i < res.length; i++) {
+            console.log("-------------------------------------------------------")
+            console.log("Product name: " + res[i].product_name);
+            console.log("Department: " + res[i].department_name);
+            console.log("Price: $" + res[i].price);
+            console.log("Product ID: " + res[i].item_id);
+            console.log("Quantity: " + res[i].stock_quantity)
         }
-      }
+    })
+}
+
+function displayLowInventory() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        //Obligatory welcome message
+        console.log("-------------------------------------------------------")
+        console.log("Here you are, m'lord")
+        //Loop through each item in the products table
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].stock_quantity < 5) {
+                console.log("-------------------------------------------------------")
+                console.log("Product name: " + res[i].product_name);
+                console.log("Department: " + res[i].department_name);
+                console.log("Price: $" + res[i].price);
+                console.log("Product ID: " + res[i].item_id);
+                console.log("Quantity: " + res[i].stock_quantity)
+            } else {
+                console.log("-------------------------------------------------------")
+                console.log(res[i] + "has plenty in stock")
+            }
+        }
     })
 }
 
 
-function endConnection() {
-    connection.end();
-}
+        function endConnection() {
+            connection.end();
+        }
